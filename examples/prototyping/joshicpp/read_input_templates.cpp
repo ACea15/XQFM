4e4fa3f762b98c68878a347ea5fe25c24e210168
Alvaro Cea <alvaro_cea@outlook.com>
2023-10-27 08:04:17 +0100

    read inputs in joshicpp

--- examples/prototyping/joshicpp/read_input_templates.cpp
+++ examples/prototyping/joshicpp/read_input_templates.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include <fstream>
+#include <ostream>
 #include <string>
 #include <sstream>
 #include <map>
 
 // Define a structure to hold the data
 struct MyData {
-    int a;
-    double b;
-    std::string c;
-};
+  int a;
+  double b;
+  std::string c;
+  MyData(int a_, float b_, std::string c_) : a(a_), b(b_), c(c_) {}
+  MyData() : a(1), b(2.5), c(std::string("ll")) {}
+  };
 
 // Utility struct for type-to-string mapping
 template <typename T>
@@ -45,10 +48,14 @@ struct TypeToString<std::string> {
 template <typename StructType>
 std::map<std::string, std::string> createVariableTypeMap() {
     std::map<std::string, std::string> variableTypes;
-
-    variableTypes["a"] = TypeToString<decltype(StructType::a)>::value();
-    variableTypes["b"] = TypeToString<decltype(StructType::b)>::value();
-    variableTypes["c"] = TypeToString<decltype(StructType::c)>::value();
+    MyData try1 = MyData(1,2.5,std::string("ll"));
+    variableTypes["a"] = TypeToString<decltype(try1.a)>::value();
+    variableTypes["b"] = TypeToString<decltype(try1.b)>::value();
+    variableTypes["c"] = TypeToString<decltype(try1.c)>::value();
+    
+    // variableTypes["a"] = TypeToString<decltype(StructTypea)>::value();
+    // variableTypes["b"] = TypeToString<decltype(StructType::b)>::value();
+    // variableTypes["c"] = TypeToString<decltype(StructType::c)>::value();
 
     return variableTypes;
 }
@@ -70,31 +77,51 @@ int main() {
     std::string line;
     while (std::getline(inputFile, line)) {
       std::string variableName, valueString;
-        //char equalSign;
+	std::istringstream iss(line);
+        while (std::getline(iss, variableName, '=')) {
+            // Remove leading and trailing spaces from variableName
+            variableName.erase(variableName.find_last_not_of(" \t") + 1);
+            variableName.erase(0, variableName.find_first_not_of(" \t"));
 
-        //std::istringstream iss(line);
-        //iss >> variableName >> equalSign >> valueString;
-        // Split the line into variable name and value
-        size_t equalPos = line.find('=');
-        if (equalPos != std::string::npos) {
-            std::string variableName = line.substr(0, equalPos);
-            std::string valueString = line.substr(equalPos + 1);
-            //data[variable] = value;
-        }
-    
+            // Remove leading and trailing spaces from the rest of the line
+            //iss >> std::ws;
+
+            if (std::getline(iss, valueString)) {
+                // Remove leading and trailing spaces from valueString
+                valueString.erase(valueString.find_last_not_of(" \t") + 1);
+                valueString.erase(0, valueString.find_first_not_of(" \t"));
+
+                // Process variableName and valueString
+                // For example, you can convert valueString to the desired type (e.g., int, double, string)
+                // and store it in a map or other data structure.
+                std::cout << "Variable Name: " << variableName << " | Value: " << valueString << std::endl;
+            } else {
+                std::cerr << "Invalid line format: " << line << std::endl;
+            }}	
+        // if (std::getline(iss, variableName, '=') && std::getline(iss, valueString)) {
+        //     // Process variableName and valueString
+        //     // For example, you can convert valueString to the desired type (e.g., int, double, string)
+        //     // and store it in a map or other data structure.
+        //     std::cout << "Variable Name: " << variableName << " | Value: " << valueString << std::endl;
+        // }
+	// else if (std::getline(iss, variableName, '=') && std::getline(iss, valueString)) {
+	//   std::cout << "Variable Name: " << variableName << " | Value: " << valueString << std::endl;
+	// }
+	// else {
+        //     std::cerr << "Invalid line format: " << line << std::endl;
+        // }
+
         // Check if the variable is in the map of known types
-        if (variableTypes.find(variableName) != variableTypes.end()) {
-            // Determine the type of the variable
-            const std::string& type = variableTypes[variableName];
-
-            // Use the type information to convert the string to the appropriate type
-            if (type == "int") {
-                myData.a = std::stoi(valueString);
-            } else if (type == "double") {
-                myData.b = std::stod(valueString);
-            } else if (type == "string") {
-                myData.c = valueString;
-            }
+        //if (variableTypes.find(variableName) != variableTypes.end()) {
+	// Determine the type of the variable
+	const std::string& type = variableTypes[variableName];	    
+	// Use the type information to convert the string to the appropriate type
+	if (type == "int") {
+	  myData.a = std::stoi(valueString);
+	} else if (type == "double") {
+	  myData.b = std::stod(valueString);
+	} else if (type == "string") {
+	  myData.c = valueString;
         }
     }
 
