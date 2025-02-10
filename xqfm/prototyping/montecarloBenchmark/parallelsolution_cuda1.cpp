33e97641a85d2327d550e5305a524f920acaaece
Alvaro Cea <alvaro_cea@outlook.com>
2025-02-10 14:38:07 +0000

    parallel cuda montecarlo, needs veification

---
+++ xqfm/prototyping/montecarloBenchmark/parallelsolution_cuda1.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <curand_kernel.h>
+#include <cuda_runtime.h>
+
+__global__ void monteCarloSDE(double* d_samples, double* d_payoff, int numPaths, int numSteps, double dt, double mu, double sigma, double x0) {
+    int pathIdx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (pathIdx >= numPaths * numSteps) return;
+
+    monteCarloPath(pathIdx, d_samples, numSteps, dt, mu, sigma, x0)
+    computed_payoff = payoff(int numSteps, int numPaths, double* d_samples, double strike)
+    *d_payoff = computed_payoff
+}
+
+__device__  void monteCarloPath(int pathIdx, double* d_samples, int numSteps, double dt, double mu, double sigma, double x0) {
+
+    // Initialize the state of the random number generator for this thread
+    curandState state;
+    curand_init(1234, pathIdx, 0, &state);
+
+    // Simulate the SDE for this path
+    double x = x0;
+    d_samples[pathIdx * numSteps] = x;
+    for (int i = 1; i < numSteps; ++i) {
+        double dW = curand_normal(&state) * sqrtf(dt); // Wiener increment
+        x += mu * dt + sigma * dW; // Euler-Maruyama update
+        // Store the final value for this path
+        d_samples[pathIdx * numSteps + i] = x;
+
+    }
+
+}
+
+__device__ double payoff(int numSteps, int numPaths, double* d_samples, double strike) {
+  double sum = 0.; 
+  for (unsigned long i = 0; i < numPaths; ++i) {
+    sum += max(strike - d_samples[numSteps * (i+1) -1], 0.);
+    }
+  return sum / numPaths;
+  }
+
+int main() {
+    auto start = std::chrono::high_resolution_clock::now();
+
+    double strike = 100.;
+    double spot = 100.0;
+    double vol = 0.3;
+    double rate = 0.03;
+    int numPaths = 10000000;
+    int numSteps = 300;
+    double T = 1.0;
+    double dt = T / numSteps;
+    double sqrt_dt = std::sqrt(dt);
+
+    double* final_payoff;
+    // Allocate Unified Memory -- accessible from CPU or GPU
+    cudaMallocManaged(&final_payoff, sizeof(double));
+
+    // Memory allocation (host and device)
+    double* h_samples = new double[numPaths * numSteps];
+    double* d_samples;
+    cudaMalloc(&d_samples, numPaths * numSteps * sizeof(double));
+
+     // Define CUDA kernel configuration
+    int threadsPerBlock = 256;
+    int numBlocks = (numPaths + threadsPerBlock - 1) / threadsPerBlock;
+
+    // Launch kernel
+    monteCarloSDE>(d_samples,
+                                                  numPaths,
+                                                  numSteps,
+                                                  dt,
+                                                  rate,
+                                                  vol,
+                                                  spot);
+
+    // Copy results back to host
+    cudaMemcpy(h_samples, d_samples, numPaths * sizeof(double), cudaMemcpyDeviceToHost);
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;      
+
+    // Clean up
+    delete[] h_samples;
+    cudaFree(d_samples);
+    cudaFree(final_payoff);
+
+
+    // std::cout << "Estimated average final value: " << averageFinalValue <<
+    // std::endl;
+    std::cout << "Totalpaths: " << numPaths << std::endl;
+    std::cout << "Duration(seconds): " << duration.count() << std::endl;
+    std::cout << "Price: " << final_payoff << std::endl;
+
+    return 0;
+}
---
+++ xqfm/prototyping/montecarloBenchmark/parallelsolution_threads1.cpp
@@ -0,0 +1,110 @@
+#include <chrono>
+#include <cmath>
+#include <iostream>
+#include <random>
+#include <stdexcept>
+#include <thread>
+#include <valarray>
+#include <vector>
+
+using vector2d = std::vector<std::vector<double>>;
+
+double payoff(unsigned long startPath, unsigned long numPaths, vector2d &S, double strike) {
+  double sum = 0.; 
+  for (unsigned long i = 0; i < numPaths; ++i) {
+    sum += std::max(strike - S[startPath + i].back(), 0.);
+
+    }
+  return sum / numPaths;
+  }
+
+void mcAnalyticalc(unsigned long startPath, unsigned long numPaths,
+                   unsigned long numSteps, double T, double dt,
+                   double sqrt_dt, double mu, double sigma, double S0,
+                   vector2d &S) {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::normal_distribution<> dis(0.0, 1.0);
+
+  for (unsigned long i = 0; i < numPaths; ++i) {
+    S[startPath + i][0] = S0;
+    for (unsigned long j = 1; j < numSteps; ++j) {
+      double dW = dis(gen) * sqrt_dt;
+      S[startPath + i][j] = S[startPath + i][j - 1] *
+          std::exp((mu - 0.5 * sigma * sigma) * dt + sigma * dW);
+    };
+  };
+}
+
+void eulerMaruyamac(unsigned long startPath, unsigned long numPaths,
+                    unsigned long numSteps, double T, double dt,
+                    double sqrt_dt, double mu, double sigma, double S0,
+                    vector2d &S) {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::normal_distribution<> dis(0.0, 1.0);
+
+  for (unsigned long i = 0; i < numPaths; ++i) {
+    S[startPath + i][0] = S0;
+    for (unsigned long j = 1; j < numSteps; ++j) {
+      double dW = dis(gen) * sqrt_dt;
+      S[startPath + i][j] = mu * S[startPath + i][j - 1] * dt +
+                            sigma * S[startPath + i][j - 1] * dW;
+    };
+  };
+}
+
+void fthread(double& output, unsigned long startPath, unsigned long numPaths,
+               unsigned long numSteps, double T, double dt,
+               double sqrt_dt, double mu, double sigma, double S0, double strike,
+               vector2d &S) {
+  mcAnalyticalc(startPath, numPaths,numSteps,  T,  dt,
+                   sqrt_dt,  mu,  sigma,  S0,
+                S);
+  output = payoff(startPath, numPaths, S, strike);
+  }
+
+int main() {
+
+  auto start = std::chrono::high_resolution_clock::now();
+
+  double strike = 100.;
+  double spot = 100.0;
+  double vol = 0.3;
+  double rate = 0.03;
+  int numPaths = 10000000;
+  int numSteps = 300;
+  double T = 1.0;
+  double dt = T / numSteps;
+  double sqrt_dt = std::sqrt(dt);
+
+  vector2d S(numPaths, std::vector<double>(numSteps, 0.));
+  unsigned int numThreads = std::thread::hardware_concurrency();
+  std::vector<double> partialSums(numThreads, 0.);
+  std::vector<std::thread> threads;
+  unsigned long pathsPerThread = numPaths / numThreads;
+  // Launch threads
+  for (unsigned int i = 0; i < numThreads; ++i) {
+    unsigned long startPath = i * pathsPerThread;
+    if (i == numThreads - 1) pathsPerThread = numPaths - startPath;
+    threads.emplace_back(fthread, std::ref(partialSums[i]), startPath, pathsPerThread, numSteps, T,
+                         dt, sqrt_dt, rate, vol, spot, strike, std::ref(S));
+  }
+
+  // Join threads
+  for (auto &t : threads) {
+    t.join();
+  }
+  double totalSum = std::accumulate(partialSums.begin(), partialSums.end(), 0.);
+  double price = totalSum / numThreads;
+  auto end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> duration = end - start;
+
+  // std::cout << "Estimated average final value: " << averageFinalValue <<
+  // std::endl;
+  std::cout << "Totalpaths: " << numPaths << std::endl;
+  std::cout << "Duration(seconds): " << duration.count() << std::endl;
+  std::cout << "Price: " << price << std::endl;
+
+  return 0;
+}
